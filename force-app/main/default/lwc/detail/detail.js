7f5f510ac527242339f4aacc343227e004fd8e64
raakk1 <raymond.van.akkeren@nn.nl>
2019-03-17 14:37:55 +0100

    added user name to detail component

--- force-app/main/default/lwc/detail/detail.js
+++ force-app/main/default/lwc/detail/detail.js
@@ -1,6 +1,10 @@
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
 import { bikes } from 'c/data';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
 
+const FIELDS = [NAME_FIELD];
 
 export default class Detail extends LightningElement {
 
@@ -9,6 +13,7 @@ export default class Detail extends LightningElement {
 
     // Private var to track @api productId
     _productId = undefined;
+
 
     // Use set and get to process the value every time it's
     // requested while switching between products
@@ -20,5 +25,12 @@ export default class Detail extends LightningElement {
     // getter for productId
     @api get productId(){
         return this._productId;
+    }
+
+    userId = Id;
+    @wire(getRecord, { recordId: '$userId', fields: FIELDS })
+    user;
+    get name() {
+        return getFieldValue(this.user.data, NAME_FIELD);
     }
 }
